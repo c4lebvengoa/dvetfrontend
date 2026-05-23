b496213810e30a0e9947c4b540bea86bdd59883b
KalelUWU <calebjuan44@gmail.com>
2026-05-22 22:04:31 -0500

    agraegué un componente de prueba

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Component1} from './components/Componente1'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Component1/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Component.jsx
@@ -0,0 +1,7 @@
+export default Component1=()=>{
+    return(
+        <>
+         <h2>Pechocha MIAAAA</h2>
+        </>
+    )
+}
